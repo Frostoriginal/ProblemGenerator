a81a28c921f14ef91bdc9b3c6234a0713f4cf188
Frost <frostsword@vp.pl>
2023-10-18 17:04:03 +0200

    Additional tests for MyCardProblem

--- ProblemGeneratorTests/MyCardProblemTests.cs
+++ ProblemGeneratorTests/MyCardProblemTests.cs
@@ -37,8 +37,8 @@ namespace ProblemGeneratorTests
             //cut.Find("li").MarkupMatches(" <li>What: unitTest</li>");
             cut.FindAll("li").MarkupMatches(
                 "<li>What: unitTest</li>" +
-                "<li>Where: xUnit</li>" +
-                "<li>Date created: 2023-10-18</li>" +
+                "<li id=\"problemWhere\">Where: xUnit</li>" +
+                $"<li>Date created: {DateTime.Now:yyyy-MM-dd}</li>" +
                 "<li>Days elapsed: 0</li>" +
                 "<li>Priority: 1</li>"
                 );
@@ -67,11 +67,65 @@ namespace ProblemGeneratorTests
             
             cut.FindAll("li").MarkupMatches(
                 "<li>What: unitTest</li>" +
-                "<li>Where: xUnit</li>" +
+                "<li id=\"problemWhere\">Where: xUnit</li>" +
                 $"<li>Date created: {TestProblem.DateCreated:yyyy-MM-dd}</li>" +
                 "<li>Days elapsed: 1</li>" +
                 "<li>Priority: 1</li>"
                 );
+
+        }
+
+        [Fact]
+        public void TestByID()
+        {
+            //Arrange
+
+            Services.AddSingleton<MyLogger>(new MyLogger());
+            Services.AddLocalization();
+
+            Problem TestProblem = new Problem()
+            {
+                What = "unitTest",
+                Where = "xUnit",
+                DateCreated = DateTime.Now.AddDays(-1),
+                problemPriority = 1,
+            };
+
+
+            var cut = RenderComponent<MyCardProblem>(parameters => parameters.Add(p => p.problem, TestProblem));
+
+            // Assert
+
+            cut.Find("#problemWhere").MarkupMatches("<li id=\"problemWhere\">Where: xUnit</li>");
+
+        }
+
+        [Fact]
+        public void TestColorCoding()
+        {
+            //Arrange
+
+            Services.AddSingleton<MyLogger>(new MyLogger());
+            Services.AddLocalization();
+
+            Problem TestProblem = new Problem()
+            {
+                What = "unitTest",
+                Where = "xUnit",
+                DateCreated = DateTime.Now.AddDays(-1),
+                problemPriority = 2,
+            };
+
+
+            var cut = RenderComponent<MyCardProblem>(parameters => parameters.Add(p => p.problem, TestProblem));
+
+            string expectedColor = "lightcoral";
+
+            var expectedHtml = $"<div class=\"card\" style=\" background:linear-gradient(135deg, #f3f6f9 80%, {expectedColor} 30%); border-radius: 4px; border-style:hidden;\">\r\n  <button class=\"btn btn-outline-secondary \"  style=\"border-style:hidden;\">\r\n    <ul style=\"text-align:left; padding:5px 5px 5px 5px;\">\r\n      <li>What: unitTest</li>\r\n      <li id=\"problemWhere\">Where: xUnit</li>\r\n      <li>Date created: 2023-10-17</li>\r\n      <li>Days elapsed: 1</li>\r\n      <li>Priority: 2</li>\r\n    </ul>\r\n  </button>\r\n</div>";
+
+            // Assert
+
+            cut.Find(".card").MarkupMatches(expectedHtml);
 
         }
     }
